aa686004df735e02aba47e22ad27e8bf62332406
unknown <nir.itsiq@post.runi.ac.il>
2024-01-24 16:39:14 +0200

    'hw6'

--- Runigram.java
+++ Runigram.java
@@ -45,8 +45,8 @@ public class Runigram {
 		// creates from the 3 colors a new Color object, and
 		// makes pixel (i,j) refer to that object.
 		//// Replace the following statement with your code.
-		for (int i = 0; i < numCols; i++) {
-			for (int j = 0; j < numRows; j++) {
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numCols; j++) {
 				int red = in.readInt();
 				int green = in.readInt();
 				int blue = in.readInt();
